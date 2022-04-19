51e57a4f6d6223d4581d60ffc938ab8a49d24555
Juan Betancourt <juancbetancourt11@gmail.com>
2022-04-19 02:04:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+fruits = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes"]
+vegetables = ["Spinach", "Kale", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+
+#Nested lists 
+print(dirty_dozen)
