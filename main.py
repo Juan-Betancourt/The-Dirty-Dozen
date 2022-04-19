d0a78c12693aab143b669f3ea9b3347d759ab300
Juan Betancourt <juancbetancourt11@gmail.com>
2022-04-19 02:12:50 +0000

    Nested list printing specific item

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-fruits = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes"]
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes"]
 vegetables = ["Spinach", "Kale", "Celery", "Potatoes"]
 
 dirty_dozen = [fruits, vegetables]
 
 #Nested lists 
-print(dirty_dozen)
+print(dirty_dozen[1][1])
